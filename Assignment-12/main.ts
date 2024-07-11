7d65053c8b2459bdb8f4752160e4862c56c244ea
TayyabAziz11 <159780590+TayyabAziz11@users.noreply.github.com>
2024-07-11 15:28:46 +0500

    update

---
+++ Assignment-12/main.ts
@@ -0,0 +1,12 @@
+// Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+// print a message to them. The text of each message should be the same,
+// but each message should be personalized with the person’s name.
+
+let names: string[] = ["Tayyab" , "Qasim" , "Umair" , "Shams"]
+
+let message: string = "Would you like to learn typescript?"
+
+console.log(names[0] + " "+ message)
+console.log(names[1] + " "+ message)
+console.log(names[2] + " "+ message)
+console.log(names[3] + " "+ message)
