ae5142eaa4afc43f21542321203588d403653998
TayyabAziz11 <159780590+TayyabAziz11@users.noreply.github.com>
2024-07-11 15:07:07 +0500

    update

---
+++ Assignment-11/main.ts
@@ -0,0 +1,8 @@
+// Store the names of a few of your friends in a array
+
+let names: string[] =["Tayyab" , "Qasim" , "Umair" , "Shams"]
+
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
